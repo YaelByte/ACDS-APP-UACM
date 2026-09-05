6ed5c32055df3e854a3696af46487e55878a9376
aleja <aleja@DESKTOP-POH28G0>
2026-09-05 12:26:36 -0600

    Se hizo el control de acceso

---
+++ src/main/java/chinsons/app/uacm/ControlAccesoController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chinsons.app.uacm;
+
+/**
+ *
+ * @author aleja
+ */
+public class ControlAccesoController {
+    
+}
